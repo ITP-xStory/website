38447020fe29ba85aa093eb31c4109c48bf63a00
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-08-02 02:40:03 +0000

    Deploy ITP-Xstory/website to github.com/ITP-Xstory/website.git:gh-pages

--- path---tags-31f19a54252dd95c6d12.js
+++ path---tags-31f19a54252dd95c6d12.js
@@ -1,2 +1,2 @@
-webpackJsonp([55702396619907],{347:function(e,n){e.exports={pathContext:{tags:["Experiment","Immersive","Machine Learning","Tool"]}}}});
+webpackJsonp([55702396619907],{348:function(e,n){e.exports={pathContext:{tags:["Experiment","Immersive","Machine Learning","Tool"]}}}});
 //# sourceMappingURL=path---tags-31f19a54252dd95c6d12.js.map
