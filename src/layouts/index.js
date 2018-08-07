cfae1c985942802d4dd2d0a158b36eda7964280a
juniorxsound <fleisher.or@gmail.com>
2018-08-07 19:42:45 -0400

    Adding favicon

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -5,6 +5,8 @@ import Helmet from 'react-helmet'
 import Header from '../components/header'
 import './index.css'
 
+import favicon from '../favicon.png'
+
 class Layout extends Component {
   constructor(props) {
     super(props)
@@ -18,6 +20,9 @@ class Layout extends Component {
           meta={[
             { name: 'description', content: 'ITP xStory is an organization dedicated to support, to promote and to publish innovative creative projects and tools in the field of digital storytelling and digital art, by ITP studets and alumni. The organization vision is to reach as many digital creators as possible, to inspire them with cutting edge ideas and creation tools, and to build a community of explorers around these ideas. The organization is sponsored and supported by NYU ITP.' },
             { name: 'keywords', content: 'Creative, ITP, xStory, NYU, Innovation, Machine Learning, Open Source, VR, AR' },
+          ]}
+          link={[
+            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
           ]}
         />
         <Header
