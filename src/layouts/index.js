ece79ca27d6c629a8813b829808e2b25f95017cd
dodiku <dda290@nyu.edu>
2018-09-14 09:14:58 -0400

    typos and grammer

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -18,16 +18,22 @@ class Layout extends Component {
         <Helmet
           title={this.props.data.site.siteMetadata.title}
           meta={[
-            { name: 'description', content: 'ITP xStory is an organization dedicated to support, to promote and to publish innovative creative projects and tools in the field of digital storytelling and digital art, by ITP studets and alumni. The organization vision is to reach as many digital creators as possible, to inspire them with cutting edge ideas and creation tools, and to build a community of explorers around these ideas. The organization is sponsored and supported by NYU ITP.' },
-            { name: 'keywords', content: 'Creative, ITP, xStory, NYU, Innovation, Machine Learning, Open Source, VR, AR' },
+            {
+              name: 'description',
+              content:
+                'ITP xStory is an organization dedicated to supporting, promoting and publishing of innovative creative projects and tools in the field of digital storytelling and digital art, by ITP students and alumni. The organization vision is to reach as many digital creators as possible, to inspire them with cutting-edge ideas and creation tools, and to build a community of explorers around these ideas. The organization is sponsored and supported by NYU ITP.',
+            },
+            {
+              name: 'keywords',
+              content:
+                'Creative, ITP, xStory, NYU, Innovation, Machine Learning, Open Source, VR, AR',
+            },
           ]}
           link={[
-            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` }
+            { rel: 'shortcut icon', type: 'image/png', href: `${favicon}` },
           ]}
         />
-        <Header
-          siteTitle={this.props.data.site.siteMetadata.title}
-        />
+        <Header siteTitle={this.props.data.site.siteMetadata.title} />
         <div
           style={{
             margin: '0 auto',
